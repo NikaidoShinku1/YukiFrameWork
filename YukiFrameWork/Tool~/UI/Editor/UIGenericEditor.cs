d3574d6cb0aee7e1ab5397b0972912e036a6c3ae
林俊豪 <1274672030@qq.com>
2024-06-27 14:58:46 +0800

    更新UI细节

--- YukiFrameWork/Tool~/UI/Editor/UIGenericEditor.cs
+++ YukiFrameWork/Tool~/UI/Editor/UIGenericEditor.cs
@@ -5,69 +5,34 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 namespace YukiFrameWork.UI
 {
     public static class UIGenericEditor 
     {
-        [MenuItem("GameObject/YukiFrameWork/Create BasePanel")]
+        [MenuItem("GameObject/YukiFrameWork/Add Framework UIRoot")]
         public static void GenericBasePanel()
-        {         
-            GameObject g = Selection.activeGameObject;
-            Canvas canvas = g?.GetComponent<Canvas>();
-            if (canvas == null)
-            {
-                var obj = new GameObject("Canvas");
-                
-#if UNITY_2021_1_OR_NEWER
-                Undo.RegisterCreatedObjectUndo(obj, "Create Canvas");
+        {
+            string sceneName = SceneManager.GetActiveScene().name;
 
-                canvas = Undo.AddComponent<Canvas>(obj);
-                Undo.AddComponent<CanvasScaler>(obj);
-                Undo.AddComponent<GraphicRaycaster>(obj);
-#else
-                canvas = obj.AddComponent<Canvas>();
-                obj.AddComponent<CanvasScaler>();
-                obj.AddComponent<GraphicRaycaster>();
+            GameObject uiRoot = SceneManager.GetActiveScene().FindRootGameObject("UIRoot");
+
+            if (uiRoot == null)
+            {
+                uiRoot = Resources.Load<GameObject>("UIRoot");
+                var o = PrefabUtility.InstantiatePrefab(uiRoot);
+                Undo.RegisterCreatedObjectUndo(o, "Add UIRoot");
+                EditorUtility.SetDirty(uiRoot);
+                AssetDatabase.SaveAssets();
+            }
+            else
+            {
+#if UNITY_EDITOR || YukiFrameWork_DEBUGFULL
+                LogKit.W("场景已经存在UiRoot");
 #endif
             }
-
-            EventSystem system = Object.FindObjectOfType<EventSystem>();
-
-            if (system == null)
-            {
-                GameObject obj = new GameObject("EventSystem");
-#if UNITY_2021_1_OR_NEWER
-                Undo.RegisterCreatedObjectUndo(obj, "Create EventSystem");
-                Undo.AddComponent<EventSystem>(obj);
-                Undo.AddComponent<StandaloneInputModule>(obj);
-#else
-                obj.AddComponent<EventSystem>();
-                obj.AddComponent<StandaloneInputModule>();
-#endif
-            }
-
-            GameObject panel = new GameObject("BasePanel");
-#if UNITY_2021_1_OR_NEWER
-            Undo.RegisterCreatedObjectUndo(panel, "Create Panel");
-            RectTransform transform = Undo.AddComponent<RectTransform>(panel);
-#else 
-            RectTransform transform = panel.AddComponent<RectTransform>();
-#endif
-            transform.SetRectTransformInfo(canvas);
-#if UNITY_2021_1_OR_NEWER
-            Undo.AddComponent<BasePanel>(panel);
-            var image = Undo.AddComponent<Image>(panel);
-#else
-            panel.AddComponent<BasePanel>();
-            var image = panel.AddComponent<Image>();
-
-            EditorUtility.SetDirty(panel);
-            AssetDatabase.SaveAssets();
-#endif
-            
-            image.color = new Color(image.color.r, image.color.g, image.color.b, 0.4f);
-        }       
+        }
     }
 }
 #endif
