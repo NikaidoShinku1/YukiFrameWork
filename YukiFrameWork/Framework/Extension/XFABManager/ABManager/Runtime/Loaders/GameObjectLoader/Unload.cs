ad9fd4758342d4a8ce0a5a20a694cacea7aaf574
林俊豪 <1274672030@qq.com>
2024-08-05 19:27:17 +0800

    同步更新XFABManager

---
+++ YukiFrameWork/Framework/Extension/XFABManager/ABManager/Runtime/Loaders/GameObjectLoader/Unload.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace XFABManager
+{
+    public class Unload : MonoBehaviour
+    {
+
+        [Header("持续时间")]
+        [Tooltip("持续时间")]
+        public float duration = 1;
+
+        [Header("忽略时间缩放")]
+        [Tooltip("忽略时间缩放")]
+        public bool ingoreTimeScale;
+
+        private float timer = 0;
+
+        private void OnEnable()
+        {
+            timer = 0;
+        }
+
+        private void Update()
+        {
+            timer += ingoreTimeScale ? Time.unscaledDeltaTime : Time.deltaTime;
+
+            if (timer >= duration) 
+            {
+                GameObjectLoader.UnLoad(gameObject);
+                timer = 0;
+            }
+             
+        }
+
+
+    }
+
+}
+
